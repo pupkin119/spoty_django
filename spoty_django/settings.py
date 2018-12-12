5e4a6ea3082b28ba50de4c841e1f5a0e4beeed9e
Alex <pupkin11999@gmail.com>
2018-12-13 00:52:58 +0300

    fix host

--- spoty_django/settings.py
+++ spoty_django/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://stormy-eyrie-79213.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'stormy-eyrie-79213.herokuapp.com']
 
 # Application definition
 
