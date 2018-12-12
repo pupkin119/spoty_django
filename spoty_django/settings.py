fd1d681191d943c22789e8f45891b50615d2389f
Alex <pupkin11999@gmail.com>
2018-12-13 00:43:06 +0300

    init

--- spoty_django/settings.py
+++ spoty_django/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://stormy-eyrie-79213.herokuapp.com/']
 
 
 # Application definition
